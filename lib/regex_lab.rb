f661a19d47dbdc5734985251732422f5cecc5247
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-18 20:34:12 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/(\d+)-(\d+)-(\d+)/)
+  if phone.matc(/(\d+)-(\d+)-(\d+)/)
     return false
   else
     return true
